7c0dabcf5dd6128718a23f6e125022ec1364ca18
Cristian Lopez <cristian.lopez09825@gmail.com>
2022-03-22 20:35:41 -0500

    Se agrego biblioteca para el LookAndFeel

--- src/run/Runner.java
+++ src/run/Runner.java
@@ -1,10 +1,19 @@
 package run;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 import controller.Controller;
+
+import javax.swing.*;
 
 public class Runner {
 
-	public static void main(String[] args) {
-		new Controller();
-	}
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
+            new Controller();
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+    }
 }
